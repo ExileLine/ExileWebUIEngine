5551c06a3e36851ee802cfd245f95b9548427adc
yangyuexiong <yang6333yyx@126.com>
2025-07-12 13:50:52 +0800

    feat:构造函数入参`browser_type`,`listening_api`逻辑;存储所有API请求与响应`log_request`,`log_response`;单元测试

--- main.py
+++ main.py
@@ -6,8 +6,9 @@
 # @Software: PyCharm
 
 import os
+import json
 import asyncio
-from typing import Union, Optional
+from typing import Union, Optional, List
 from platform import system
 
 from playwright.async_api import Browser, FrameLocator, ElementHandle
@@ -23,9 +24,11 @@ class ExileWebUIEngine:
             headless: bool = False,
             debugger_address: bool = False,
             debugger_address_url: str = "http://127.0.0.1:9222",
+            browser_type: str = "chrome",
             init_url: str = "https://github.com/ExileLine",
             width: int = None,
             height: int = None,
+            listening_api: List[str] = None
     ) -> None:
         """
 
@@ -33,9 +36,11 @@ class ExileWebUIEngine:
         :param headless: 是否使用无界面
         :param debugger_address: 是否操作已启动的浏览器
         :param debugger_address_url: 结合 `debugger_address` 使用
+        :param browser_type: 浏览器类型 chrome,firefox
         :param init_url: 浏览器初始页面地址
         :param width: 浏览器初始宽
         :param height: 浏览器初始高
+        :param listening_api: 监听的`api`或页面`path`
         """
         self.is_debug = is_debug
         self.headless = headless
@@ -48,29 +53,94 @@ class ExileWebUIEngine:
         self.page_list = []
         self.page_list_len = None
         self.page_obj_list = []  # [{'index': 0, 'title': 'GitHub', 'url': 'https://github.com/'}...]
+        self.browser_type = browser_type
         self.init_url = init_url
         self.width = width
         self.height = height
 
+        self.listening_api = listening_api
+
+        # 存储所有API请求与响应
+        self.api_requests = []
+        self.api_responses = []
+        self.cookies = []
+
         # MACOS: /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222
         # MACOS: /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --user-data-dir=/tmp/chrome-debug-profile
         # Windows: ...
+
+    async def get_cookies(self):
+        """获取操作接口的`cookies`"""
+
+        try:
+            cookies = self.page.context.cookies()
+            return cookies
+        except BaseException as e:
+            return {
+                "func": "get_cookies",
+                "message": f"`get_cookies` 错误 {e}"
+            }
+
+    async def log_request(self, request):
+        """监听请求"""
+
+        for api in self.listening_api:
+            if api in request.url:
+                self.api_requests.append(request)
+
+    async def log_response(self, response):
+        """监听响应"""
+
+        for api in self.listening_api:
+            if api in response.url:
+                self.api_responses.append(response)
 
     async def start(self):
         """启动"""
 
         self.playwright = await async_playwright().start()
 
+        if self.browser_type not in ["chrome", "firefox"]:
+            raise ValueError("browser_type not in ['chrome', 'firefox']")
+
         if self.debugger_address:
-            self.browser = await self.playwright.chromium.connect_over_cdp(self.debugger_address_url)
+            if self.browser_type == "chrome":
+                self.browser = await self.playwright.chromium.connect_over_cdp(self.debugger_address_url)
+            elif self.browser_type == "firefox":
+                self.browser = await self.playwright.firefox.connect_over_cdp(self.debugger_address_url)
+            else:
+                pass
+
             await self.handle_context_page()
             await self.reload_pages()
         else:
-            self.browser = await self.playwright.chromium.launch(headless=self.headless)
-            self.context = await self.browser.new_context(ignore_https_errors=True)
+            rule_list = [
+                "--disable-http-cache",  # 禁用缓存
+                "--disable-features=SameSiteByDefaultCookies"  # 临时解决Cookie策略问题
+            ]
+            if self.browser_type == "chrome":
+                self.browser = await self.playwright.chromium.launch(headless=self.headless, args=rule_list)
+            elif self.browser_type == "firefox":
+                self.browser = await self.playwright.firefox.launch(headless=self.headless, args=rule_list)
+            else:
+                pass
+
+            self.context = await self.browser.new_context(
+                ignore_https_errors=True,  # 忽略HTTPS错误
+                storage_state=None,  # 关键：每次启动使用全新存储状态
+                bypass_csp=True,  # 强制覆盖所有内容安全策略
+            )
+
+            await self.out_logs(f"listening_api: {self.listening_api}")
+            if self.listening_api:
+                self.context.on("request", self.log_request)
+                self.context.on("response", self.log_response)
+
             self.page = await self.context.new_page()
+
             if self.width and self.height:
-                await self.page.set_viewport_size({"width": self.width, "height": self.height})
+                await self.page.set_viewport_size({"width": self.width, "height": self.height})  # type: ignore
+
             await self.page.goto(self.init_url)
             await self.show_title()
 
@@ -80,6 +150,7 @@ class ExileWebUIEngine:
         if self.is_debug:
             input("输入回车后关闭浏览器...")
 
+        await self.context.close()
         await self.browser.close()
         await self.playwright.stop()
 
@@ -113,6 +184,15 @@ class ExileWebUIEngine:
             }
             await self.out_logs(f"页签对象: {page_obj}")
             self.page_obj_list.append(page_obj)
+
+    async def page_wait(self, timeout: int | float):
+        """
+        等待所有请求完成
+        :param timeout:
+        :return:
+        """
+
+        await self.page.wait_for_timeout(timeout)
 
     async def browser_connet(self) -> bool:
         """获取浏览器连接状态"""
@@ -167,17 +247,31 @@ class ExileWebUIEngine:
     async def check_element(self, element: str):
         """检查元素"""
 
-    async def action_input(self, element: str, value: str):
-        """输入"""
+    async def action_input(self, t: str, element: str, value: str):
+        """
+        输入
+        :param t: 元素类型 XPATH, CSS, ...
+        :param element: 元素值
+        :param value:   输入值
+        :return:
+        """
 
         await self.check_element(element)
-        await self.page.fill(element, value)
 
-    async def action_click(self, element: str):
+        if t in ["xpath", "XPATH"]:
+            await self.page.locator(f"xpath={element}").fill(value)
+        else:
+            await self.page.fill(element, value)
+
+    async def action_click(self, t: str, element: str):
         """点击"""
 
         await self.check_element(element)
-        await self.page.click(element)
+
+        if t in ["xpath", "XPATH"]:
+            await self.page.click(f"xpath={element}")
+        else:
+            await self.page.click(element)
 
     async def test(self):
         """test"""
@@ -185,11 +279,102 @@ class ExileWebUIEngine:
         await self.start()
         await self.stop()
 
+    async def test2(self):
+        """test2"""
+
+        await self.start()
+
+        await self.action_input(
+            t="xpath",
+            element="""/html/body/div[1]/div/div[2]/form/div[1]/div[2]/div/div/div/input""",
+            value="super"
+        )
+        await self.action_input(
+            t="xpath",
+            element="""/html/body/div[1]/div/div[2]/form/div[2]/div[2]/div/div/div/input""",
+            value="123456"
+        )
+        await self.action_click(
+            t="xpath",
+            element="""/html/body/div[1]/div/div[2]/form/div[3]/div[2]/div/div/div/button[1]"""
+        )
+
+        await self.page_wait(1000)
+
+        # 输出结果
+        print("\n=== 捕获的API请求 ===")
+        for request in self.api_requests:
+            # print(request.__dir__())
+            request_dict = {
+                "method": request.method,
+                "url": request.url,
+                "headers": request.headers,
+                "data": request.post_data,
+                "json_data": request.post_data_json,
+            }
+
+            # print(request.method)
+            # print(request.url)
+            # print(request.headers)
+            # print(request.post_data)
+            # print(request.post_data_json)
+
+            print(json.dumps(request_dict, ensure_ascii=False))
+
+        print("\n=== API响应数据 ===")
+        for response in self.api_responses:
+            # print(response.__dir__())
+            response_dict = {
+                "url": response.url,
+                "http_code": response.status,
+                "http_message": response.status_text,
+                "headers": response.headers,
+                "all_headers": await response.all_headers(),
+                # "response_json": await response.json(),
+                # "response_text": await response.text(),
+                # "response_body": await response.body()
+            }
+            try:
+                response_dict["response_json"] = await response.json()
+            except Exception as e:
+                print(f"无法解析JSON响应: {str(e)}")
+
+            try:
+                response_dict["response_text"] = await response.text()
+            except Exception as e:
+                print(f"无法解析TEXT响应: {str(e)}")
+
+            # print(response.url)
+            # print(response.status)
+            # print(response.status_text)
+            # print(response.headers)
+            # print(await response.all_headers())
+            # print(await response.json())
+            # print(await response.text())
+            # print(await response.body())
+
+            print(json.dumps(response_dict, ensure_ascii=False))
+
+            print("\n=== API响应Cookies ===")
+
+            cookies = response_dict.get("all_headers", {}).get("set-cookie", "")
+            print(cookies.split(";"))
+
+        await self.stop()
+
 
 if __name__ == '__main__':
+    # engine = ExileWebUIEngine(
+    #     is_debug=True,
+    #     headless=False,
+    #     init_url="https://www.metatuple.com/"
+    # )
+    # asyncio.run(engine.test())
+
     engine = ExileWebUIEngine(
         is_debug=True,
-        headless=True,
-        init_url="https://www.metatuple.com/"
+        headless=False,
+        init_url="http://localhost:3200/",
+        listening_api=["/api/account/login"]
     )
-    asyncio.run(engine.test())
+    asyncio.run(engine.test2())
